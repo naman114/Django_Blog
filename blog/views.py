b919fd1d6edaa8a5497236a9484ce14637400f89
naman114 <namangogia2001@gmail.com>
2021-03-16 02:43:25 +0530

    Before Template Inheritance

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+# List of dictionaries
+posts = [
+    {
+        "author": "Naman Gogia",
+        "title": "Blog Post 1",
+        "content": "Blog Post 1 Content",
+        "date_posted": "March 16, 2021",
+    },
+    {
+        "author": "CoreyMS",
+        "title": "Blog Post 2",
+        "content": "Blog Post 2 Content",
+        "date_posted": "March 17, 2021",
+    },
+]
+
 
 # Function to handle traffic from our home. Returns what the user will see
-
-
 def home(request):
-    return HttpResponse("<h1>Blog Home</h1>")
+    context = {"posts": posts}
+    return render(request, "blog/home.html", context)
+
+
+# data is passed to the template using context. From there, we can use posts key to access the list of dictionaries i.e. posts
 
 
 def about(request):
-    return HttpResponse("<h1>Blog About</h1>")
+    return render(request, "blog/about.html", {"title": "About"})
