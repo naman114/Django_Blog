7ce1b4e3a652127acf235554dc2d8acbeda1a26e
Naman Gogia <namangogia2001@gmail.com>
2021-05-02 22:55:10 +0530

    Video 11

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import (
     ListView,
     DetailView,
@@ -7,6 +7,7 @@ from django.views.generic import (
     DeleteView,
 )
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 from .models import Post
 
 # Function to handle traffic from our home. Returns what the user will see
@@ -23,6 +24,18 @@ class PostListView(ListView):
     template_name = "blog/home.html"  # <app>/<model>_<viewtype>.html
     context_object_name = "postsbro"
     ordering = ["-date_posted"]
+    paginate_by = 5
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = "blog/user_posts.html"  # <app>/<model>_<viewtype>.html
+    context_object_name = "postsbro"
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get("username"))
+        return Post.objects.filter(author=user).order_by("-date_posted")
 
 
 class PostDetailView(DetailView):
